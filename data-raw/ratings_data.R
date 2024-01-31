ea3e155138c36cd15ff8a68f823a68fea1ab268b
zhgarfield <zhgarfield@gmail.com>
2024-01-31 12:44:47 +0100

    Adding data file. Updating package files.

--- data-raw/ratings_data.R
+++ data-raw/ratings_data.R
@@ -1,3 +1,18 @@
 ## code to prepare `ratings_data` dataset goes here
 
+
+# Load libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(readr)
+
+
+# Read data ---------------------------------------------------------------
+
+ratings_data <- read_csv("data-raw/ratings_data.csv")
+
+
+
+# End ---------------------------------------------------------------------
+
 usethis::use_data(ratings_data, overwrite = TRUE)
